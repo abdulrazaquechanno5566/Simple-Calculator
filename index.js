ca9deef91db7e4865ca5ed8863857060a0074acd
Abdul Razaque <abdulrazaquechanno5566@gmail.com>
2024-03-26 02:02:27 +0500

    add the console at the end

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ else if (answer.operator === "Division") {
 }
 else {
     console.log("Please select valid operator");
+    console.log("THE END !!");
 }
--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter the first number", type: "number", name: "firstNumber" },
+  { message: "Enter the second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perform operation",
     type: "list",
@@ -28,6 +28,7 @@ if (answer.operator === "Addition") {
 } 
 else {
   console.log("Please select valid operator")
+  console.log("THE END !!")
 }
 
 
